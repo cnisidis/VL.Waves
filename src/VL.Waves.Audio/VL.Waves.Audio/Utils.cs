8e9d9b19b45382a4a8cbe603ac98bed04bc6f540
cnisidis <makeithappen@nisidis.com>
2023-01-08 15:29:26 +0200

    src - dll

---
+++ src/VL.Waves.Audio/VL.Waves.Audio/Utils.cs
@@ -0,0 +1,23 @@
+﻿
+using VL.Audio;
+using NWaves;
+using NWaves.Signals;
+using System.Collections.Generic;
+
+namespace VL.Waves.Audio
+{
+    public class Utils
+    {
+        
+        
+        public static DiscreteSignal ToDiscrete(int samplingRate, AudioSignal Signal)
+        {
+            IReadOnlyList<float> buffer = new List<float>();
+            int lastIntCount = 0;
+            Signal.GetInternalReadBuffer(out buffer, out lastIntCount);
+            DiscreteSignal DSignal = new DiscreteSignal(samplingRate, buffer);
+           
+            return DSignal;
+        }
+    }
+}

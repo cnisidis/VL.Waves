ffa550e9d0f098a18b64eb6864e6ca4dea76cb69
cnisidis <makeithappen@nisidis.com>
2023-01-19 16:44:03 +0200

    Add feature: can convert AudioSignal from Vl.Audio to discrete

--- src/VL.Waves.Audio/VL.Waves.Audio/Utils.cs
+++ src/VL.Waves.Audio/VL.Waves.Audio/Utils.cs
@@ -12,12 +12,23 @@ namespace VL.Waves.Audio
         
         public static DiscreteSignal ToDiscrete(int samplingRate, AudioSignal Signal)
         {
+           
             IReadOnlyList<float> buffer = new List<float>();
             int lastIntCount = 0;
             Signal.GetInternalReadBuffer(out buffer, out lastIntCount);
             DiscreteSignal DSignal = new DiscreteSignal(samplingRate, buffer);
            
             return DSignal;
+        }
+
+        public static AudioSignal FromDiscrete(DiscreteSignal discrete)
+        { 
+            AudioSignalGeneratorRegion<>
+            AudioSignal Signal = null;
+            float[] samples = discrete.Samples;
+            List<SigParamBase> sigParam = AudioSignal.GetInputParams(Signal).ToList();
+            sigParam.
+            
         }
     }
 }

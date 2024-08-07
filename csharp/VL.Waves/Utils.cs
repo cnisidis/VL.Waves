25b025c36a215b111d96e0c861d99592b61c9fa3
cnisidis <makeithappen@nisidis.com>
2024-08-07 11:46:58 +0300

    Updates and Adds Audio tools in C# project
    Updates lib
    Updates DSP NEW test

--- csharp/VL.Waves/Utils.cs
+++ csharp/VL.Waves/Utils.cs
@@ -1,10 +1,10 @@
 ///For examples, see:
 ///https://thegraybook.vvvv.org/reference/extending/writing-nodes.html#examples
 //using CommunityToolkit.HighPerformance;
-using NWaves;
+
 using NWaves.Signals;
-using NWaves.Signals.Builders.Base;
-using System.Linq;
+using VL.Lib.Collections;
+
 
 namespace VL.Waves;
 
@@ -34,17 +34,80 @@ public static class Utils
         
         return newSignal;
     }
-    /*
-    public static List<float> OccurenciesAverage(IEnumerable<KeyValuePair<int, float>> values)
+    public static class AudioUtils
     {
-        int prev_pair_key = 0;
-        
-        for (int i = 0; i<values.Count(); i++)
+        public static float HzToMel(float hz)
         {
-            List<float> newValues = new List<float>();
-            
-            newValues.Add();
+            return 2595 * (float)Math.Log10(1 + hz / 700);
         }
+
+        // Method to convert Mel to Hz
+        public static float MelToHz(float mel)
+        {
+            return 700 * (float)(Math.Pow(10, mel / 2595) - 1);
+        }
+        public static Spread<double[]> CreateMelFilterBank(int sr, int nFft, int nMels, float fMin, float fMax)
+        {
+            // Calculate Mel frequencies
+            Spread<float> melPoints = Enumerable.Range(0, nMels + 2)
+                                            .Select(i => HzToMel(fMin + i * (fMax - fMin) / (nMels + 1)))
+                                            .ToSpread();
+
+            Spread<float> hzPoints = melPoints.Select(i => MelToHz(i)).ToSpread();
+
+            // Convert Hz to FFT bin numbers
+            Spread<int> binPoints = hzPoints.Select(hz => (int)Math.Floor((nFft + 1) * hz / sr)).ToSpread();
+
+            // Initialize the filter bank
+            List<double[]> filters = new List<double[]>(nMels);
+            for (int m = 1; m <= nMels; m++)
+            {
+                double[] filter = new double[nFft];
+
+                int fMMinus = binPoints[m - 1];
+                int fM = binPoints[m];
+                int fMPlus = binPoints[m + 1];
+
+                for (int k = fMMinus; k < fM; k++)
+                {
+                    filter[k] = (double)(k - fMMinus) / (fM - fMMinus);
+                }
+                for (int k = fM; k < fMPlus; k++)
+                {
+                    filter[k] = (double)(fMPlus - k) / (fMPlus - fM);
+                }
+
+                filters.Add(filter);
+            }
+
+            return filters.ToSpread();
+        }
+
+        public static Spread<float> ApplyMelFilterBank(Spread<float> magnitudes, Spread<double[]> melFilters)
+        {
+            int fftSize = magnitudes.Count;
+            int melFilterCount = melFilters.Count;
+
+            float[] melSpectrum = new float[melFilterCount];
+
+            for (int i = 0; i < melFilterCount; i++)
+            {
+                double[] filter = melFilters[i];
+                double filterSum = 0.0;
+
+                for (int j = 0; j < filter.Length; j++)
+                {
+                    if (j < fftSize) // Ensure we are within bounds
+                    {
+                        filterSum += (float)magnitudes[j] * (float)filter[j];
+                    }
+                }
+
+                melSpectrum[i] = (float)filterSum;
+            }
+
+            return melSpectrum.ToSpread();
+        }
+
     }
-    */
 }
